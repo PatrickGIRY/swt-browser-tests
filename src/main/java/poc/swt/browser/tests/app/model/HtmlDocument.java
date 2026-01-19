80c7e4c62ee7fe637e7ef00d36c7145c7de2e03a
patrickgiry <patrickgiry@gmail.com>
2026-01-19 23:13:01 +0100

    Add model `HtmlDocument`

---
+++ src/main/java/poc/swt/browser/tests/app/model/HtmlDocument.java
@@ -0,0 +1,20 @@
+package poc.swt.browser.tests.app.model;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public record HtmlDocument(String url, String content) {
+
+    public HtmlDocument() {
+        this("about:blank", "");
+    }
+
+    public HtmlDocument(String url, String content) {
+        try {
+            this.url = new URL(url).toString();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        this.content = content != null ? content : "";
+    }
+}
