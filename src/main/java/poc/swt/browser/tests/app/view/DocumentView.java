10dcb880476b07220529bb7ac67594978f500804
patrickgiry <patrickgiry@gmail.com>
2026-01-20 17:12:19 +0100

    Use document view model and event bus

--- src/main/java/poc/swt/browser/tests/app/view/DocumentView.java
+++ src/main/java/poc/swt/browser/tests/app/view/DocumentView.java
@@ -2,21 +2,31 @@ package poc.swt.browser.tests.app.view;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.browser.ProgressEvent;
+import org.eclipse.swt.browser.ProgressListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
+import poc.swt.browser.tests.app.eventbus.EventBus;
+import poc.swt.browser.tests.app.viewmodel.CurrentUrlUpdated;
 import poc.swt.browser.tests.app.viewmodel.DocumentViewModel;
+import poc.swt.browser.tests.app.viewmodel.Event;
+
+import java.util.function.Consumer;
 
 public class DocumentView extends Composite {
 
     private final Browser browser;
     private final  Text urlText;
     private final DocumentViewModel viewModel;
+    private final EventBus<Event> eventBus;
+    private final Consumer<CurrentUrlUpdated> currentUrlUpdatedConsumer;
 
     public DocumentView(Composite parent, int style) {
         super(parent, style);
-        this.viewModel = new DocumentViewModel();
+        this.eventBus = new EventBus<>();
+        this.viewModel = new DocumentViewModel(eventBus);
         setLayout(new GridLayout(1, false));
 
         // Champ de texte pour l'URL
@@ -25,7 +35,7 @@ public class DocumentView extends Composite {
         urlText.setText(viewModel.currentUrl());
         urlText.addListener(SWT.KeyDown, e -> {
             if (e.keyCode == SWT.CR) {
-                loadUrl();
+                viewModel.setCurrentUrl(urlText.getText());
             }
         });
 
@@ -33,13 +43,30 @@ public class DocumentView extends Composite {
         browser = new Browser(this, SWT.NONE);
         browser.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
         browser.setUrl(viewModel.currentUrl());
+
+        currentUrlUpdatedConsumer = this::onCurrentUrlUpdated;
+        eventBus.subscribe(CurrentUrlUpdated.class, currentUrlUpdatedConsumer);
+
+        browser.addProgressListener(new  ProgressListener() {
+            @Override
+            public void changed(ProgressEvent event) {
+
+            }
+
+            @Override
+            public void completed(ProgressEvent event) {
+                viewModel.setNewContent(browser.getUrl(), browser.getText());
+            }
+        });
     }
 
-    private void loadUrl() {
-        String url = urlText.getText();
-        if (url != null && !url.isEmpty()) {
-            viewModel.loadUrl(url);
-            browser.setUrl(viewModel.currentUrl());
-        }
+    private void onCurrentUrlUpdated(CurrentUrlUpdated event) {
+        browser.setUrl(event.newUrl());
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        eventBus.unsubscribe(CurrentUrlUpdated.class, currentUrlUpdatedConsumer);
     }
 }
--- src/main/java/poc/swt/browser/tests/app/viewmodel/DocumentViewModel.java
+++ src/main/java/poc/swt/browser/tests/app/viewmodel/DocumentViewModel.java
@@ -1,20 +1,31 @@
 package poc.swt.browser.tests.app.viewmodel;
 
+import poc.swt.browser.tests.app.eventbus.EventBus;
 import poc.swt.browser.tests.app.model.HtmlDocument;
 
 public class DocumentViewModel {
     private HtmlDocument document;
 
-    public DocumentViewModel() {
+    private final EventBus<Event> eventBus;
+
+    public DocumentViewModel(EventBus<Event> eventBus) {
+        this.eventBus = eventBus;
         this.document = new HtmlDocument();
     }
 
-    public void loadUrl(String url) {
-        this.document = new HtmlDocument(url);
+    public void setCurrentUrl(String newUrl) {
+        if (newUrl != null && !newUrl.isEmpty()) {
+            final var oldUrl = document.url();
+            this.document = new HtmlDocument(newUrl);
+            eventBus.publish(new CurrentUrlUpdated(oldUrl, newUrl));
+        }
     }
 
     public String currentUrl() {
         return document.url();
     }
 
+    public void setNewContent(String url, String content) {
+        document = new HtmlDocument(url, content);
+    }
 }
