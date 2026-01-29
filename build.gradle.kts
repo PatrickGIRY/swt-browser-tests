4ddf0dd763bab44d58368288b60055213e429494
patrickgiry <patrickgiry@gmail.com>
2026-01-29 13:09:33 +0100

    Add mockito dependency for test

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,7 +63,10 @@ dependencies {
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.21.0")
+    testImplementation("org.mockito:mockito-core:5.21.0")
 }
 
 tasks.test {
